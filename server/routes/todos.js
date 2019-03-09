d6e59d3205cc2d55bb2e47b29f5a1959441e7b96
gbarrbrown <gbb7899@gmail.com>
2019-03-10 11:17:04 +1300

    mock payload

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -3,12 +3,19 @@ const router = express.Router()
 
 // GET /api/v1/todos
 router.get('/', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            task: "task 1",
+            completed: false
+        }
+    ])
 
 })
 
 // POST /api/v1/todos
 router.post('/', (req, res) => {
-    
+
 })
 
 module.exports = router
