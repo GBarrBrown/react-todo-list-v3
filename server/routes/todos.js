cd6e42b48eb6bf5746feb3326f8f5febf5c07637
gbarrbrown <gbb7899@gmail.com>
2019-03-10 11:01:30 +1300

    create routes/todos.js & configure express to use todoRoutes

---
+++ server/routes/todos.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+// GET /api/v1/todos
+router.get('/', (req, res) => {
+
+})
+
+// POST /api/v1/todos
+router.post('/', (req, res) => {
+    
+})
+
+module.exports = router
