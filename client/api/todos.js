5b4e82f1fd8b52df74d9f743b5696d3d8031002e
gbarrbrown <gbb7899@gmail.com>
2019-03-11 12:48:33 +1300

    add catch to getTodos

--- client/api/todos.js
+++ client/api/todos.js
@@ -1,8 +1,9 @@
 import request from 'superagent'
 
 export function getTodos() {
-    return request.get('/api/v1/todos/')
-    .then(res => {
-        return res.body
+    return request.get('/api/v1/todos')
+    .then(res => res.body)
+    .catch(err => {
+        console.log('ERROR!', err)
     })
 }
